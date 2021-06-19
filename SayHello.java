798cd0233d8814430115c71777ef38c44fb44cd1
ddqlyld <86143279+ddqlyld@users.noreply.github.com>
2021-06-19 15:59:05 +0800

    Add files via upload

---
+++ SayHello.java
@@ -0,0 +1,18 @@
+package 线程.作业;
+
+public class SayHello extends Thread{
+    @Override
+    public void run() {
+        for (int i=1;i<=20;i++){
+            System.out.println(Thread.currentThread().getName()+"你好");
+        }
+    }
+
+    public static void main(String[] args) {
+        SayHello m1=new SayHello();
+        m1.start();
+        for (int j=1;j<=20;j++){
+            System.out.println(Thread.currentThread().getName()+"Hello");
+        }
+    }
+}
